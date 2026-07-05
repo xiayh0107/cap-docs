327d5f92d890f96184702ec4b07e2f4ee14c174c
Yonghe Xia <xiayonghe@westlake.edu.cn>
2026-07-05 21:42:57 +0800

    Export digest text helpers

--- reference/python/cap_digest/__init__.py
+++ reference/python/cap_digest/__init__.py
@@ -1,5 +1,11 @@
 """Experimental CAP-Digest reference implementation."""
 
 from .assembler import assemble_table, validate_response
+from .text import parse_digest_text, validate_manifest_text_consistency
 
-__all__ = ["assemble_table", "validate_response"]
+__all__ = [
+    "assemble_table",
+    "validate_response",
+    "parse_digest_text",
+    "validate_manifest_text_consistency",
+]
