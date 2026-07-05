75c067997a57371d3b6dd4ded1ca5a720a12434c
Yonghe Xia <xiayonghe@westlake.edu.cn>
2026-07-05 21:43:49 +0800

    Export follow-up helpers

--- reference/python/cap_digest/__init__.py
+++ reference/python/cap_digest/__init__.py
@@ -1,11 +1,14 @@
 """Experimental CAP-Digest reference implementation."""
 
 from .assembler import assemble_table, validate_response
+from .followup import gate_requests, validation_result
 from .text import parse_digest_text, validate_manifest_text_consistency
 
 __all__ = [
     "assemble_table",
     "validate_response",
+    "validation_result",
+    "gate_requests",
     "parse_digest_text",
     "validate_manifest_text_consistency",
 ]
